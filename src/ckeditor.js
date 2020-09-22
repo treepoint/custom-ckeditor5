13909388754cadfea3d86e4aa9660bd4dca39329
treepoint <owner-of-all-real@ya.ru>
2020-09-22 12:24:15 +0500

    add some font features

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -101,7 +101,7 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	table: {
-		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+		contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'ru'
